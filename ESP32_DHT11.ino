042dfd4ace43c2d4c3e321553d5e51573d308f84
Sufian Kaki Aslam <sufiankaki@gmail.com>
2020-07-23 19:07:08 +0530

    Create ESP32_DHT11.ino

---
+++ ESP32_DHT11.ino
@@ -0,0 +1,35 @@
+#include "DHT.h"
+
+#define DHTPIN 2     
+
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.println(f);
+}
